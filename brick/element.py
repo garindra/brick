cff17bd41e82fc0ca2d4f4cc6603fde944abd886
Garindra Prahandono <garindraprahandono@gmail.com>
2013-02-09 11:53:00 -0800

    cleanup of methods

--- brick/element.py
+++ brick/element.py
@@ -30,52 +30,42 @@ class Element(object):
 
         if utils.is_context_manager(context_manager):
             with context_manager:
-                result = self._process_buffer(self.after_render_buffer(self.render_buffer()))
+                result = self.process_buffer(self.after_render_buffer(self.render_buffer()))
         else:
-            result = self._process_buffer(self.after_render_buffer(self.render_buffer()))
+            result = self.process_buffer(self.after_render_buffer(self.render_buffer()))
 
         return result
+    
+    def process_buffer(self, buf):
+        
+        return self.after_process_buffer(self._process_buffer(
+                                            self.before_process_buffer(buf)))
 
     def _process_buffer(self, buf):
         
-        self.before_process_buffer(buf)
-
         #If self.render returns nothing, then just return empty string
         if buf is None:
             return ""
 
         #If the result is a string, then just directly return that
-        if isinstance(buf, str):
+        elif isinstance(buf, str):
             return buf 
         
         #If the result is an instance of Element, then call the render_buffer method of it.
         #And return that.
-        if isinstance(buf, Element):
+        elif isinstance(buf, Element):
             return buf.render()
 
-        if isinstance(buf, list):
-            return self._merge_buffer(buf)
-        
-    def _merge_buffer(self, buf):
-        temp_buf = []
+        elif isinstance(buf, list):
+            return "".join(map(self._process_buffer, buf))
 
-        for element in buf:
-
-            if isinstance(element, Element):
-                temp_buf.append(element.render())
-
-            elif isinstance(element, str):
-                temp_buf.append(element)
-
-            else:
-                raise Exception("Only Element or str instance are allowed in render")
-
-        return ''.join(temp_buf)
+        else:
+            raise Exception("Only Element or str instance are allowed in render")
 
     def render_buffer(self):
         """ 
             Implementing the render method is simple; return a string, a list
-            of strings, or an instance of Element subclass.
+            of strings, or instances of Element subclass.
         """
         raise NotImplementedError("`render_buffer` method should be implemented for %s" % (
                                     self.__class__.__name__))
