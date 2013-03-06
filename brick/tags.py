63b1b5b8fd7e3a153d3e646edeb6ae7af8ac9978
Garindra Prahandono <garindraprahandono@gmail.com>
2013-03-05 21:18:57 -0800

    make Link and Meta self-closing tags

--- brick/tags.py
+++ brick/tags.py
@@ -1,11 +1,11 @@
 from .utils import make_tag, text
 
-_tags_to_build = ('A', 'Div', 'Span', 'Script', 'Link', 'Title', 'Head', 'Body', 
-                  'Html', 'Head', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P', 'Meta',
+_tags_to_build = ('A', 'Div', 'Span', 'Script', 'Title', 'Head', 'Body', 
+                  'Html', 'Head', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P',
                   'Textarea', 'Form', 'Ul', 'Ol', 'Li', 'Code', 'Blockquote', 'Em', 
                   'Strong', 'Select', 'Option', 'Iframe')
 
-_self_closing_tags_to_build = ('Input', 'BR', 'Img')
+_self_closing_tags_to_build = ('Input', 'BR', 'Img', 'Meta', 'Link')
 
 for tag in _tags_to_build:
     #define the tag in the current module
